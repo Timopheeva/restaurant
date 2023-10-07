c4d3f5a2373e417b84b56cf1fb750c06a3e729d4
Olga <dn150791kos@gmail.com>
2023-10-07 14:02:02 +0300

    слайдер карусель

--- src/js/main.js
+++ src/js/main.js
@@ -345,46 +345,119 @@ class MenuCard {
 
     //Slider
 
-    const slides = document.querySelectorAll('.offer__slide'),
-          prev = document.querySelector('.offer__slider-prev'),
-          next = document.querySelector('.offer__slider-next'),
-          total = document.querySelector('#total'),
-          current = document.querySelector('#current');
+    // const slides = document.querySelectorAll('.offer__slide'),
+    //       prev = document.querySelector('.offer__slider-prev'),
+    //       next = document.querySelector('.offer__slider-next'),
+    //       total = document.querySelector('#total'),
+    //       current = document.querySelector('#current');
 
 
-    let slideIndex = 1;
+    // let slideIndex = 1;
 
-    showSlides(slideIndex);
+    // showSlides(slideIndex);
 
     
-    total.textContent = getZero(slides.length);
+    // total.textContent = getZero(slides.length);
 
 
-    function showSlides (n) {
-        if(n > slides.length) {
-            slideIndex = 1;
-        } 
+    // function showSlides (n) {
+    //     if(n > slides.length) {
+    //         slideIndex = 1;
+    //     } 
 
-        if (n < 1) {
-            slideIndex = slides.length;
-        }
-        slides.forEach( item => item.style.display = 'none');
+    //     if (n < 1) {
+    //         slideIndex = slides.length;
+    //     }
+    //     slides.forEach( item => item.style.display = 'none');
         
-        slides[slideIndex -1].style.display = 'block';
+    //     slides[slideIndex -1].style.display = 'block';
 
-        current.textContent = getZero(slideIndex);
+    //     current.textContent = getZero(slideIndex);
 
+    // }
+    // function plusSlides(n) {
+    //     showSlides(slideIndex +=n);
+    // }
+
+    // prev.addEventListener('click', ()=> {
+    //     plusSlides(-1);
+    // });
+
+    // next.addEventListener('click', ()=> {
+    //     plusSlides(1);
+    // });
+
+
+
+// Slider carousel
+
+
+
+const slides = document.querySelectorAll('.offer__slide'),
+prev = document.querySelector('.offer__slider-prev'),
+next = document.querySelector('.offer__slider-next'),
+total = document.querySelector('#total'),
+current = document.querySelector('#current'),
+slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+slidesField = document.querySelector('.offer_slider-inner'),
+width = window.getComputedStyle(slidesWrapper).width;
+
+
+let slideIndex = 1;
+let offset = 0;
+
+    total.textContent = getZero(slides.length);
+    current.textContent = getZero(slideIndex);
+
+
+slidesField.style.width = 100 * slides.length + '%';
+slidesField.style.display = 'flex';
+slidesField.style.transition = '0.5s all';
+
+slidesWrapper.style.overflow = 'hidden';
+
+
+slides.forEach(slide => {
+    slide.style.width = width;
+
+});
+
+  next.addEventListener('click', ()=> {
+    if (offset == +width.slice(0, width.length -2) * (slides.length -1)) {
+        offset = 0;
+    } else {
+        offset += +width.slice(0, width.length -2);
     }
-    function plusSlides(n) {
-        showSlides(slideIndex +=n);
-    }
 
-    prev.addEventListener('click', ()=> {
-        plusSlides(-1);
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
+        slideIndex = 1;
+    } else {
+        slideIndex++;
+    }
+    current.textContent = getZero(slideIndex);
+
+    
     });
 
-    next.addEventListener('click', ()=> {
-        plusSlides(1);
+
+    prev.addEventListener('click', ()=> {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length -2) * (slides.length -1);
+            
+        } else {
+            offset -= +width.slice(0, width.length -2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+        
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+        current.textContent = getZero(slideIndex);
     });
 
 
